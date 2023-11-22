bc136b5425d06b418b4a76ec3c19e575eb0d5047
Muhammad Fakhri Musyaffa <mfakhrim643@gmail.com>
2023-11-22 17:36:01 +0700

    tambahan fitur profil dan intent

--- app/src/main/java/il/massive/gea_rent/ui/TambahProdukActivity.kt
+++ app/src/main/java/il/massive/gea_rent/ui/TambahProdukActivity.kt
@@ -1,12 +1,32 @@
 package il.massive.gea_rent.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import il.massive.gea_rent.R
 
-class TambahProdukActivity : AppCompatActivity() {
+class TambahProdukActivity : AppCompatActivity(), View.OnClickListener  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tambah_produk)
+        val btnSimpanProduk: Button = findViewById(R.id.simpan_produk)
+        btnSimpanProduk.setOnClickListener(this)
+        val btnBackToko:ImageButton = findViewById(R.id.backTokoSaya)
+        btnBackToko.setOnClickListener(this)
+    }
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.simpan_produk-> {
+                val intent = Intent(this@TambahProdukActivity, TokoSayaActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.backTokoSaya-> {
+                val intent = Intent(this@TambahProdukActivity, TokoSayaActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/il/massive/gea_rent/ui/TokoSayaActivity.kt
+++ app/src/main/java/il/massive/gea_rent/ui/TokoSayaActivity.kt
@@ -1,12 +1,70 @@
 package il.massive.gea_rent.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import il.massive.gea_rent.R
+import il.massive.gea_rent.databinding.ActivityTokoSayaBinding
+import il.massive.gea_rent.ui.adapters.TokoSayaAdapter
+import il.massive.gea_rent.ui.model.TokoSayaModel
 
-class TokoSayaActivity : AppCompatActivity() {
+class TokoSayaActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var adapter: TokoSayaAdapter
+    private lateinit var binding: ActivityTokoSayaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_toko_saya)
+        binding = ActivityTokoSayaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setTokoSayaAdapter()
+        val btnTambahProduk: Button = findViewById(R.id.produksewa_tambah)
+        btnTambahProduk.setOnClickListener(this)
+        val btnBackToko: ImageButton = findViewById(R.id.backToko)
+        btnBackToko.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.produksewa_tambah-> {
+                val intent = Intent(this@TokoSayaActivity, TambahProdukActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.backToko-> {
+                val intent = Intent(this@TokoSayaActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun setTokoSayaAdapter() {
+        val dataList: MutableList<TokoSayaModel> = mutableListOf()
+
+        judul().forEachIndexed { index, judul ->
+            dataList.add(TokoSayaModel(image()[index], judul, tersedia()[index], harga()[index]))
+        }
+
+        // Inisialisasi adapter setelah membuat dataList
+        adapter = TokoSayaAdapter(dataList, this)
+
+        // Atur RecyclerView
+        binding.recylerview.layoutManager = LinearLayoutManager(this)
+        binding.recylerview.adapter = adapter
+    }
+
+    private fun judul(): Array<String> = resources.getStringArray(R.array.judul)
+    private fun tersedia(): Array<String> = resources.getStringArray(R.array.tersedia)
+    private fun harga(): Array<String> = resources.getStringArray(R.array.harga)
+    private fun image(): List<Int> = listOf(
+        R.drawable.img_item1,
+        R.drawable.img_item2,
+        R.drawable.img_item3,
+        R.drawable.img_item4,
+        R.drawable.img_item5,
+        R.drawable.img_item6,
+        R.drawable.img_item7
+    )
 }
